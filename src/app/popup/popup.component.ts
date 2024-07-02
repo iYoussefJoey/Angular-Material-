5509d4fa770c652e3bd5708fd3d9eab150603c6e
joey151298 <57530419+joey151298@users.noreply.github.com>
2024-07-02 14:38:04 +0300

    Update popup.component.ts

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
@@ -26,8 +26,11 @@ import {
   templateUrl: './popup.component.html',
   styleUrl: './popup.component.css',
 })
-export class PopupComponent {
-  constructor(private dialog: MatDialogRef<PopupComponent>) {}
+export class PopupComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) data:any ,private dialog: MatDialogRef<PopupComponent>) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   closePopUp(){
     this.dialog.close()
   }
